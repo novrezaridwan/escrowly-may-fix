ebde58dc86aedbfa4a7367dc718d90c193517989
Novreza Ridwansyah <Novrezaridwansyah@gmail.com>
2025-05-16 21:17:48 +0700

    finalv2

--- src/pages/ServicePage/ServicePage8.jsx
+++ src/pages/ServicePage/ServicePage8.jsx
@@ -183,23 +183,66 @@ const ServicePage8 = () => (
                                         >
                                             <SwiperSlide style={{ width: 'auto' }}>
                                                 <ServiceCard
-                                                    icon="/img/icon-hiw-3.svg"
-                                                    title="General Escrow Services "
-                                                    text="Secure transactions for goods, services, and contracts with full protection"
+                                                    icon="/img/service-1.svg"
+                                                    title="Crypto Escrow Services"
+                                                    text="Secure crypto escrow transactions for goods, services, and contracts with full protection."
+                                                    url="/crypto-escrow-services"
                                                 />
                                             </SwiperSlide>
                                             <SwiperSlide style={{ width: 'auto' }}>
                                                 <ServiceCard
-                                                    icon="/img/icon-hiw-3.svg"
+                                                    icon="/img/service-2.svg"
                                                     title="Domain Name Escrow"
-                                                    text="Ensure a safe and smooth transfer of domain ownership with escrow protection"
+                                                    text="Guarantee a safe and smooth transfer of domain ownership with escrow protection."
+                                                    url="/domain-name-escrow"
                                                 />
                                             </SwiperSlide>
                                             <SwiperSlide style={{ width: 'auto' }}>
                                                 <ServiceCard
-                                                    icon="/img/icon-hiw-3.svg"
+                                                    icon="/img/service-3.svg"
                                                     title="Motor Vehicle Escrow "
-                                                    text="Buy or sell vehicles confidently with funds securely held until transfer"
+                                                    text="Buy or sell vehicles confidently with crypto funds securely held until transfer."
+                                                    url="/motor-vehicle-escrow"
+                                                />
+                                            </SwiperSlide>
+                                            <SwiperSlide style={{ width: 'auto' }}>
+                                                <ServiceCard
+                                                    icon="/img/service-4.svg"
+                                                    title="Milestone Transactions"
+                                                    text="Release payments in stages for large projects with escrow, ensuring trust and accountability."
+                                                    url="/milestone-transactions-escrow"
+                                                />
+                                            </SwiperSlide>
+                                            <SwiperSlide style={{ width: 'auto' }}>
+                                                <ServiceCard
+                                                    icon="/img/service-5.svg"
+                                                    title="Online Goods & Digital Items"
+                                                    text="Secure online purchases by using crypto escrow, including software and other digital goods."
+                                                    url="/online-goods-digital-items-escrow"
+                                                />
+                                            </SwiperSlide>
+                                            <SwiperSlide style={{ width: 'auto' }}>
+                                                <ServiceCard
+                                                    icon="/img/service-6.svg"
+                                                    title="Stock & Investment Escrow "
+                                                    text="Secure stock trades and asset agreements with trusted crypto escrow services."
+                                                    url="/stock-investment-escrow"
+                                                />
+                                            </SwiperSlide>
+                                            <SwiperSlide style={{ width: 'auto' }}>
+                                                <ServiceCard
+                                                    icon="/img/service-7.svg"
+                                                    title="Luxury Items Escrow "
+                                                    text="Buy and sell high-value items like jewelry, watches, and collectibles with assurance."
+                                                    url="/luxury-items-escrow"
+                                                />
+                                            </SwiperSlide>
+                                            <SwiperSlide style={{ width: 'auto' }}>
+                                                <ServiceCard
+                                                    icon="/img/service-8.svg"
+                                                    title="Real Estate Escrow"
+                                                    text="Protect real estate transfers with crypto escrow until all terms are fulfilled."
+                                                    url="/real-estate-escrow"
                                                 />
                                             </SwiperSlide>
                                         </Swiper>
